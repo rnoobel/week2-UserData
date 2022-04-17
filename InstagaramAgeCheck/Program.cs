6b6f3e08b4b8ba55b1287f0844f30ba19ed95b69
Raido Noobel <rnoobel@tktk.ee>
2022-04-17 22:34:02 +0300

    Control Flow

---
+++ InstagaramAgeCheck/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InstagaramAgeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaj avanust;
+            //programm kontrollib, kas kasutaja on vähemalt 13 aastat vana või vanem;
+            //kui kasutaja on vähemalt 13 aastat vana või vanem, siis ta võib instagram'i kasutada
+            //kui kasutaja on noorem kui 13, siis ta on liiga noor.
+
+            Console.WriteLine("Palun sisesta oma vanus:");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            if (userAge > 13) 
+            {
+                Console.WriteLine("Oled piisavalt vana, et Instagram'i kontot luua.");
+            }
+            else if(userAge < 13)
+            {
+                Console.WriteLine("Oled liiga noor, et Instagram'i kontot luua.");
+            }
+            else
+            {
+                Console.WriteLine("Oledki 13, nüüd saad endale Instagram'i kontot luua.");
+            }
+
+            Console.WriteLine("Kena päeva!");
+
+        }
+    }
+}
---
+++ UserAge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaasta;
+            //programm kuvab kasutaja sünniaasta konsoolis;
+            //"oled sündinud aastal {yearOfBirth}"
+
+            Console.WriteLine("Palun sisesta oma vanus:");
+
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            int yearOfBirth = 2022 - userAge;
+
+            //Console.WriteLine("Oled sündinud aastal" + yearOfBirth + ".");
+
+            //string interpolation;
+
+            Console.WriteLine($"Oled sündinud aastal {yearOfBirth}.");
+        }
+    }
+}
